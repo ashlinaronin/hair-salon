ea3abc61c4caa8f136024b8c82537ac3d5227ac7
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-21 10:55:26 -0700

    Write deleteAll test in Client, pass automatically

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -141,6 +141,25 @@
             $this->assertEquals([$client1], $result);
         }
 
+        function test_deleteAll()
+        {
+            //Arrange
+            $stylist = new Stylist("Diane", "5035528959", "MPB", false);
+            $stylist->save();
+
+            $client1 = new Client("Steven", "5412232442", $stylist->getId());
+            $client1->save();
+            $client2 = new Client("Matthew", "5097869876", $stylist->getId());
+            $client2->save();
+
+            //Act
+            Client::deleteAll();
+            $result = Client::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
 
 
 
