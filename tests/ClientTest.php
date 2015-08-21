e76fda21ac457fba3acea20f8fd0bb96b8a1a957
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-21 10:38:47 -0700

    Write tests for other getters in Client and pass

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -36,6 +36,38 @@
             $this->assertEquals($client_name, $result);
         }
 
+        function test_getPhone()
+        {
+            //Arrange
+            $client_name = "Bill Withers";
+            $client_phone = "5415134876";
+            $stylist = new Stylist("Diane", "5035528959", "MPB", false);
+            $stylist->save();
+            $client = new Client($client_name, $client_phone, $stylist->getId());
+
+            //Act
+            $result = $client->getPhone();
+
+            //Assert
+            $this->assertEquals($client_phone, $result);
+        }
+
+        function test_getStylistId()
+        {
+            //Arrange
+            $client_name = "Bill Withers";
+            $client_phone = "5415134876";
+            $stylist = new Stylist("Diane", "5035528959", "MPB", false);
+            $stylist->save();
+            $client = new Client($client_name, $client_phone, $stylist->getId());
+
+            //Act
+            $result = $client->getStylistId();
+
+            //Assert
+            $this->assertEquals($stylist->getId(), $result);
+        }
+
 
 
     }
